e49e346eb3818c5606603ece7631036230de2d91
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-05-27 19:04:02 +0600

    Add create method in Controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,19 +1,23 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def index
         @articles = Article.all
     end
+
     def show
         @article = Article.find(params[:id])
     end
+
     def new
         @article = Article.new
     end
+
     def create
-        @article = Article.new(
-            title: params[:article][:title],
-            body: params[:article][:body])
-        @article.save 
-        redirect_to article_path(@article)  
+        @article = Article.new(article_params)
+        @article.save
+    
+        redirect_to article_path(@article)
     end
 
 end
