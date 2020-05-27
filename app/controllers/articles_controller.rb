e399a40c3d3810e64cb18ccd15c28d721c1852ab
hillarioh <okerioh@gmail.com>
2020-05-27 13:52:55 +0300

    add form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,8 @@ class ArticlesController < ApplicationController
     end
     def show
         @article = Article.find(params[:id])
-
+    end
+    def new
+        @article = Article.new
     end
 end
