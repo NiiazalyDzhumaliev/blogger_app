852bd005cdc50bc7dd83930323f57e01d593f42f
Niiazaly Dzhumaliev <diamondas1993@gmail.com>
2020-05-28 14:39:51 +0600

    Add tag list method to create a tag array

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,4 +4,10 @@ class Article < ApplicationRecord
   def tag_list
     tags.collect(&:name).join(', ')
  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(',').collect { |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
